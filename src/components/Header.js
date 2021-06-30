4c29895e776c9abdc92edc3dacee95716436ed5c
Mriganka Bora <mrigankaborah16@gmail.com>
2021-06-30 20:14:18 +0530

    changed Navigation with Tabs of material-ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,39 @@
 import React from "react";
-import {Link } from "react-router-dom"
-function Header() {
+import { Link } from "react-router-dom"
+import { Grid, Paper, Tabs, Tab } from '@material-ui/core'
+import { useState } from "react";
+
+const navStyle= {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent:"center",
+    height:"15vh",
+    alignItems:"center",
+    paddingTop:20,
+    backgroundColor: '#220000',
+    color: '#fff',
+}
+
+const paperStyle = { display:"flex", flexDirection:"column",padding:0, height: '15vh', width: "100%", margin: 0 }
+
+const Header = () => {
+  const [value,setValue] = useState(0);
+  const handleChange = (event, value) => {
+    setValue(value);
+  };
   return (
-    <header>
-      <Link className="homepage-link" to="/">
-      <h1>Users</h1>
-      </Link>
-      
-      <Link className="users-link" to="./users">
-        <h1>view users</h1>
-      </Link>
-    </header>
+    <Paper style ={paperStyle}elevation={3}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor={"secondary"}
+              centered
+              style={navStyle}
+            >
+              <Tab label="Home" to="/" component={Link} />
+              <Tab label="Users" to="/users" component={Link} />
+            </Tabs>
+    </Paper>
   );
 }
 
