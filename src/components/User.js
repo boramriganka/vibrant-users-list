5e3a62d01beeda33dcf138f7c4194f37a56d67e2
Mriganka Bora <mrigankaborah16@gmail.com>
2021-07-01 19:24:59 +0530

    card styles changed,used material-ui and css grid

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import LockIcon from '@material-ui/icons/Lock';
+import Paper from '@material-ui/core/Paper'
 import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
-import Paper from '@material-ui/core/Paper';
+import { green, pink } from '@material-ui/core/colors';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 250,
+    backgroundColor: theme.palette.background.paper,
+  },
+  pink: {
+    color: theme.palette.getContrastText(pink[500]),
+    backgroundColor: pink[500],
+  },
+  green: {
+    color: '#fff',
+    backgroundColor: green[500],
+  },
+  field:{
+    display:'flex',
+    justifyContent:"space-between"
+  },
+  btn:{
+    height:"1.5rem",
+    width:"1.5rem",
+    outline:"none",
+    border:"none",
+    textDecoration:"none"
+  },
+  deleteBtn:{
+    height:"1.5rem",
+    width:"1.5rem",
+    '&:hover':{
+      backgroundColor:"red"
+    }
+  }
+}));
+
 
 function User(props) {
   function onBtnClick(event) {
@@ -9,19 +52,42 @@ function User(props) {
 
     props.delete(props.id);
   }
-
+  const {name,email,password} = props;
+  const classes = useStyles();
   return (
-    <Paper className="User">
-      <h1><span>Name : </span>{props.name}</h1>
-      <h2><span>Email : </span>{props.email}</h2>
-      <h2><span>Password : </span>{props.password}</h2>
-
-      <button onClick={onBtnClick}>
-        <IconButton aria-label="delete">
+    
+    <Paper elevation={3}className={classes.root}>
+    <List component="nav" aria-label="main mailbox folders">
+      <ListItem className={classes.field}>
+      <Avatar className={classes.green}>
+        <ContactPhoneIcon />
+      </Avatar>
+      {name}
+      </ListItem>
+      <ListItem className={classes.field}>
+      <Avatar className={classes.pink}>
+        < ContactMailIcon />
+      </Avatar>
+      {email}
+      </ListItem>
+      <ListItem  className={classes.field}>
+      <Avatar className={classes.pink}>
+        <LockIcon/>
+      </Avatar>
+      {password}
+      </ListItem>
+    </List>
+    <button className={classes.btn}  onClick={onBtnClick}>
+        <Avatar className={classes.deleteBtn}   aria-label="delete">
           <DeleteIcon />
-        </IconButton>
+        </Avatar>
       </button>
-    </Paper>
+    <Divider />
+    
+   
+  </Paper>
+
+
   );
 }
 
